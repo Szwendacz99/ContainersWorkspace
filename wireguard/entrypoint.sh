47bc959c533ce46ab46c7977b63870db1ca9a618
Maciej Lebiest <szwendacz@proton.me>
2023-08-01 17:40:45 +0200

    fix wireguard stopping

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+stopcontainer()
+{
+    echo "removing wg0 interface"
+    wg-quick down /data/wg0.conf
+    echo goodbye
+    exit 0
+}
+
+trap stopcontainer SIGINT
+
 for file in /setup.d/*;
 do
     echo "Executing setup file $file";
     bash -c "$file";
 done
 
-wg-quick up /data/wg0.conf && sleep infinity
-echo "removing wg0 interface"
-wg-quick down /data/wg0.conf
+wg-quick up /data/wg0.conf
+
+while true; do sleep 5; done
+
